a13652b682653abbd065d17d45b39af82f0e1bd2
VirginiaH <virginianyawira50@gmail.com>
2023-08-31 15:29:45 +0300

    commit two

--- lib/Provider/provider_home.dart
+++ lib/Provider/provider_home.dart
@@ -1,2 +1,25 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Item{}
+
+class CartModel extends ChangeNotifier{
+  // Internal, private state of the cart.
+  final List<Item> _items=[];
+
+  // An unmodifiable view of the items in the cart.
+  UnmodifiableListView<Item> get items => UnmodifiableListView(_items);
+
+  // The current total price of all items (assuming all items cost Ksh. 42).
+  int get totalPrice => _items.length * 42;
+// Adds [item] to cart. This and [removeAll] are the only ways to modify the
+// cart from the outside.
+   void addItem(Item item) {
+     _items.add(item);
+     notifyListeners(); // This call tells the widgets that are listening to this model to rebuild.
+   }
+
+}
 
