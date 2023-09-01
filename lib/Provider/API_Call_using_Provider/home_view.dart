8dd2a68d7b00eab4770ac6e99544afc7923c32db
VirginiaH <virginianyawira50@gmail.com>
2023-09-01 14:00:57 +0300

    commit 5.1

---
+++ lib/Provider/API_Call_using_Provider/home_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProviderWithApi extends StatefulWidget {
+  const ProviderWithApi({super.key});
+
+  @override
+  State<ProviderWithApi> createState() => _ProviderWithApiState();
+}
+
+class _ProviderWithApiState extends State<ProviderWithApi> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
